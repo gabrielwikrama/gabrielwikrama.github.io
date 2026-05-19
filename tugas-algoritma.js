2e56619d29589bff5daea6ef2deb6234a470b93c
gabrielwikrama <gabrielwikrama@gmail.com>
2026-05-19 22:18:29 +0700

    update

---
+++ tugas-algoritma.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    mermaid.initialize({
+        startOnLoad: true, 
+        theme: 'dark',
+        themeVariables: {
+            primaryColor: '#6366f1',
+            primaryTextColor: '#fff',
+            primaryBorderColor: '#a855f7',
+            lineColor: '#ec4899',
+            secondaryColor: '#1e1e2e',
+            tertiaryColor: '#1e1e2e'
+        }
+    });
+});
